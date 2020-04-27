a8baa9650514cb8b2ca3ead432e7b7e0dcb9c7dd
Dinh Phu <ndp17hp@gmail.com>
2020-04-27 11:51:28 +0700

    them file

---
+++ thuc hanh - lop hinh chu nhat/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int tuoi = 31;
+        if (tuoi == 31){
+            System.out.println("thi anh dang tan con gai hieu truong");}
+            else{
+            System.out.println("tau dang di hoc cap 3");
+        }
+    }
+
+}
+
+
+
